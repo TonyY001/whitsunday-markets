e5a2945e5c33e93df33d52e996ab5c4d78428109
Tony Young <12010@coderacademy.edu.au>
2022-05-14 11:46:38 +1000

    Added variables and extra data to seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,11 +9,14 @@
 Stall.destroy_all
 Product.destroy_all
 
-Stall.create(name: "Tropical Photography")
-Stall.create(name: "Handmade by Shereen")
-Stall.create(name: "Airlie Pies")
+tropical_photography = Stall.create(name: "Tropical Photography")
+handmade_gifts = Stall.create(name: "Handmade Gifts")
+yummy_pies = Stall.create(name: "Yummy Pies")
 
 
-Product.create(name: "Sunset Photograph", description:"Picture taken at sunset", price: "$20")
-Product.create(name: "Beach Photograph", description:"Picture taken at the beach", price: "$20")
-Product.create(name: "Rainforest Photograph", description:"Picture taken of the rainforest", price: "$20")
+tropical_photography.products.create(name: "Sunset Photograph", description:"Picture taken at sunset", price: "$20")
+tropical_photography.products.create(name: "Beach Photograph", description:"Picture taken at the beach", price: "$20")
+handmade_gifts.products.create(name: "Shell Bracelet", description:"Bracelet made from local shells", price: "$10")
+handmade_gifts.products.create(name: "Driftwood Carving", description:"Carving made from driftwood", price: "$30")
+yummy_pies.products.create(name: "Steak Pie", description:"Delicious pie made with chunky steak and gravy", price: "$5")
+yummy_pies.products.create(name: "Chicken Pie", description:"Delicious pie made with chicken and gravy", price: "$5")
